ff667594f43696c2fe395edaf8dde255f15b1e6d
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-12 13:10:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { TiktokDL } from './plugins/DL-tiktok.js';
 import { ytbmp3downloader, ytbmp4downloader } from './plugins/DL-youtube.js';
 import { InstagramDL } from './plugins/DL-instagram.js';
 import { SpotifyDL } from './plugins/DL-spotify.js';
-import { twitterDownloader } from './plugins/DL-x.js';
+import { googleDriveDownloader } from './plugins/DL-gdrive.js';
 
 // Ekspor semua fitur di sini
-export { chatGPT, blackBoxChat, TiktokDL, ytbmp3downloader, ytbmp4downloader, InstagramDL, SpotifyDL, twitterDownloader };
+export { chatGPT, blackBoxChat, TiktokDL, ytbmp3downloader, ytbmp4downloader, InstagramDL, SpotifyDL, googleDriveDownloader };
