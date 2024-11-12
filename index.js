83d08dbc0e1f581a20ed4544ed64f854d50b970c
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-12 12:50:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { chatGPT } from './plugins/Ai-ChatGPT.js';
 import { blackBoxChat } from './plugins/Ai-Blackbox.js';
 import { TiktokDL } from './plugins/DL-tiktok.js';
+import { ytbmp3downloader, ytbmp4downloader } from './plugins/ytDownloader.js';
 
 // Ekspor semua fitur di sini
-export { chatGPT, blackBoxChat, TiktokDL };
+export { chatGPT, blackBoxChat, TiktokDL, ytbmp3downloader, ytbmp4downloader };
