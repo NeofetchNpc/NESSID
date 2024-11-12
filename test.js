7c43cf2de0bbd6fb1f8cbf80d965e7d3380a580f
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-12 10:08:29 +0700

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-import { chatGPT } from 'neastooid';
-
-(async () => {
-    const prompt = "Hi";
-    const response = await chatGPT(prompt);
-
-    console.log("Response from chatGPT:", response); // Menampilkan respons di log terminal
-})();
