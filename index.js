6348a86ef818d7605be216337e5c93a11584474a
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-12 17:09:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { SpotifyDL } from './plugins/DL-spotify.js';
 import { twitterDownloader } from './plugins/DL-x.js';
 import { googleDriveDownloader } from './plugins/DL-gdrive.js';
 import { megaDl } from './plugins/DL-mega.js';
+import { nhentaiSearch } from './plugins/AN-nhentai.js';
 
 // Ekspor semua fitur di sini
-export { chatGPT, blackBoxChat, TiktokDL, ytbmp3downloader, ytbmp4downloader, InstagramDL, SpotifyDL, twitterDownloader, googleDriveDownloader, megaDl };
+export { chatGPT, blackBoxChat, TiktokDL, ytbmp3downloader, ytbmp4downloader, InstagramDL, SpotifyDL, twitterDownloader, googleDriveDownloader, megaDl, nhentaiSearch };
