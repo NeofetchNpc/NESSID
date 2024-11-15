6154224f7cf7b7e134a3f417b3f36df2cd51057f
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-16 01:04:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import { googleDriveDownloader } from './plugins/DL-gdrive.js';
 import { megaDl } from './plugins/DL-mega.js';
 import { nhentaiSearch } from './plugins/AN-nhentai.js';
 import { sfwAnime } from './plugins/AN-sfwnime.js';
+import { charAnimeInfo } from './plugins/AN-charinfo.js';
 
 // Ekspor semua fitur di sini
-export { chatGPT, blackBoxChat, TiktokDL, ytbmp3downloader, ytbmp4downloader, InstagramDL, SpotifyDL, twitterDownloader, googleDriveDownloader, megaDl, nhentaiSearch, sfwAnime };
+export { chatGPT, blackBoxChat, TiktokDL, ytbmp3downloader, ytbmp4downloader, InstagramDL, SpotifyDL, twitterDownloader, googleDriveDownloader, megaDl, nhentaiSearch, sfwAnime, charAnimeInfo };
