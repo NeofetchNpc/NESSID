f9cb32980503074954a407c7ed1ac3326d8c5bf2
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-16 00:44:38 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-import { chatGPT } from './index.js';
-
-(async () => {
-    const prompt = "Hi";
-    const response = await chatGPT(prompt);
-
-    console.log("Response from chatGPT:", response); // Menampilkan respons di log terminal
-})();
-
 import { sfwAnime } from './index.js';
 
 (async () => {
