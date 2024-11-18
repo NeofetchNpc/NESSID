e21638a7f283532aa9479ead442aaf42fc208854
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-18 07:20:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { chatGPT } from './plugins/Ai-ChatGPT.js';
 import { blackBoxChat } from './plugins/Ai-Blackbox.js';
 import { TiktokDL } from './plugins/DL-tiktok.js';
-import { ytbmp3downloader, ytbmp4downloader } from './plugins/DL-youtube.js';
+import { ytbmp3downloader } from './plugins/DL-ytmp3.js';
+import { ytbmp4downloader } from './plugins/DL-ytmp4.js';
 import { InstagramDL } from './plugins/DL-instagram.js';
 import { SpotifyDL } from './plugins/DL-spotify.js';
 import { twitterDownloader } from './plugins/DL-x.js';
@@ -12,4 +13,18 @@ import { sfwAnime } from './plugins/AN-sfwnime.js';
 import { charAnimeInfo } from './plugins/AN-charinfo.js';
 
 // Ekspor semua fitur di sini
-export { chatGPT, blackBoxChat, TiktokDL, ytbmp3downloader, ytbmp4downloader, InstagramDL, SpotifyDL, twitterDownloader, googleDriveDownloader, megaDl, nhentaiSearch, sfwAnime, charAnimeInfo };
+export { 
+    chatGPT, 
+    blackBoxChat, 
+    TiktokDL, 
+    ytbmp3downloader, 
+    ytbmp4downloader, 
+    InstagramDL, 
+    SpotifyDL, 
+    twitterDownloader, 
+    googleDriveDownloader, 
+    megaDl, 
+    nhentaiSearch, 
+    sfwAnime, 
+    charAnimeInfo 
+};
