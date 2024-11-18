e9504a85fc5d3a9e608144dd23cb4be4bcd3b258
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-18 07:46:58 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,7 @@
-import { charAnimeInfo } from './index.js';
+import { gemini } from './index.js';
 
 (async () => {
-    const query = "Vermail";
-    const response = await charAnimeInfo(query);
-    console.log("Character Results:", response);
+    const prompt = "Hello, how are you today?";
+    const response = await gemini(prompt);
+    console.log("AI Response:", response);
 })();
-
-// MP3
-import { ytbmp3downloader } from './index.js';
-
-ytbmp3downloader('https://youtu.be/v37ECJeIjBw?si=xp0jsR6ejdT1hhKf')
-    .then(data => console.log('MP3 Data:', data))
-    .catch(error => console.error('Error:', error));
-// MP4
-import { ytbmp4downloader } from './index.js';
-
-ytbmp4downloader('https://youtu.be/v37ECJeIjBw?si=xp0jsR6ejdT1hhKf')
-    .then(data => console.log('MP4 Data:', data))
-    .catch(error => console.error('Error:', error));
