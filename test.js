0db41ba7ee40d48fc3efe4b89c7a9fbff8980bc9
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-18 07:12:46 +0700

    Update test.js

--- test.js
+++ test.js
@@ -5,3 +5,15 @@ import { charAnimeInfo } from './index.js';
     const response = await charAnimeInfo(query);
     console.log("Character Results:", response);
 })();
+
+import { ytbmp3downloader, ytbmp4downloader } from './index.js';
+
+(async () => {
+    const youtubeUrl = "https://youtu.be/v37ECJeIjBw?si=xp0jsR6ejdT1hhKf"; // Ganti dengan URL YouTube asli
+
+    const mp3Response = await ytbmp3downloader(youtubeUrl);
+    console.log("Response from YouTube MP3 Downloader:", mp3Response);
+
+    const mp4Response = await ytbmp4downloader(youtubeUrl);
+    console.log("Response from YouTube MP4 Downloader:", mp4Response);
+})();
