96c5cc23452ca4e39ab3c14b7425efb882ce5357
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-19 04:26:28 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
-import { SpotifyDL } from './index.js';
+import { PinterestSearch } from './index.js';
 
 (async () => {
-    const spotifyUrl = "https://open.spotify.com/track/0vPisMufQ5Ht1M5LaUlohP?si=YkBHmqoCSImsF7EKG_ntQw"; 
-    const response = await SpotifyDL(spotifyUrl);
-
-    console.log("Response from Spotify Downloader:", response); 
+    try {
+        const query = "anime";
+        const response = await PinterestSearch(query);
+        console.log(response);
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
 })();
