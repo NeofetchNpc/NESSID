505cf665a94f026210c825b2d50060f8dbcad416
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-20 20:42:58 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,5 @@
-import { allInOneDownload } from './index.js';
+import { telegraph } from 'ironman-api';
 
 (async () => {
-  try {
-    const url = 'https://soundcloud.com/yusup909/dj-untungnya-hidup-harus-tetap';
-    const result = await allInOneDownload(url);
-    console.log(result);
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
+  console.log(await telegraph('./img.png'));
 })();
