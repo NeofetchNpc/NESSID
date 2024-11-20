df846edf0ab0c8ad9b141860eed6cc2d5e746c95
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-20 18:42:36 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
-import { gemini } from './index.js';
+import { mediafireDl } from './index.js';
 
 (async () => {
-    const prompt = "Hello, how are you today?";
-    const response = await gemini(prompt);
-    console.log(response);
+  try {
+    const url = 'https://www.mediafire.com/file/yrdb8o89nt9t0qc/1SoundAbangPilihygMana.mp3/file';
+    const data = await mediafireDl(url);
+    console.log(data);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 })();
