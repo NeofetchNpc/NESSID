35624d699843c0d0321c7de50932558e7a02bfad
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-26 17:50:39 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,38 +1,15 @@
-/*import { SpotifyDL } from './index.js';
+import { sfwWaifuV2 } from './index.js';
 
 (async () => {
-    const spotifyUrl = "https://open.spotify.com/track/1D8kctV7MtI4rWZhbYeaF9?si=3Q2-VQvzSHSGhVso4zRvAg&utm_source=copy-link"; 
-    const response = await SpotifyDL(spotifyUrl);
-
-    console.log(response); 
-})();
-
-import { bStasionDl } from './index.js';
-
-const url = 'https://www.bilibili.tv/id/video/4793260970610688?bstar_from=bstar-web.homepage.trending.all';
-
-(async () => {
-  try {
-    const result = await bStasionDl(url);
+    const result = await sfwAnime();
     console.log(result);
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
-})();*/
-
-/*import { chatGPT } from './index.js';
-
-(async () => {
-    const prompt = "Hello, how are you today?";
-    const response = await chatGPT(prompt);
-    console.log(response);
-})();*/
-
-import { InstagramDL } from './index.js';
-
-(async () => {
-    const instagramUrl = "https://www.instagram.com/reel/DCoGdJITgJZ/?igsh=aGY3cWF5dXhjejBq"; 
-    const response = await InstagramDL(instagramUrl);
-
-    console.log(response); 
 })();
+
+/* List Func V2
+- sfwWaifuV2 ( Random Waifu )
+- sfwXwaifuV2 ( Random Waifu +18 )
+- sfwNekoV2 ( Random Neko )
+- sfwXnekoV2 ( Randon Neko +18 )
+- sfwMeguminV2 ( Random Megumin )
+- sfwAwooV2 ( Random Awoo )
+*/
