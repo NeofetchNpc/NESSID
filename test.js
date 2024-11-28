a2c061be8540943182239a77671df3040db4a1f2
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-28 17:37:59 +0700

    Update test.js

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ import { sfwCosplayerV3 } from './index.js';
 import { baLogo } from './index.js'; // Sesuaikan dengan lokasi module Anda
 
 (async () => {
-    const logoText = "My Custom Logo";
+    const logoText = "Kanjut Kanjut";
     const response = await baLogo(logoText);
     console.log(response);
 })();
