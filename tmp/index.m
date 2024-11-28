d270e2e9aebee7a27dfdfe85342511195d1dbf70
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-28 18:04:53 +0700

    Create index.m

---
+++ tmp/index.m
@@ -0,0 +1,11 @@
+# `📌 Random Anime Features </>`
+
+1. Random Waifu
+```js
+import { sfwAnime } from 'neastooapi';
+
+(async () => {
+    const result = await sfwAnime();
+    console.log(result);
+})();
+```
