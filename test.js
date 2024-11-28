540893e38e8cf7c3ac959eaf07699378518906b3
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-28 17:39:32 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,3 @@
-import { sfwCosplayerV3 } from './index.js';
-
-(async () => {
-    const result = await sfwCosplayerV3();
-    console.log(result);
-})();
-
-/* List Func V2
-- sfwWaifuV2 ( Random Waifu )
-- sfwXwaifuV2 ( Random Waifu +18 )
-- sfwNekoV2 ( Random Neko )
-- sfwXnekoV2 ( Randon Neko +18 )
-- sfwMeguminV2 ( Random Megumin )
-- sfwAwooV2 ( Random Awoo )
-*/
-
 import { baLogo } from './index.js'; // Sesuaikan dengan lokasi module Anda
 
 (async () => {
