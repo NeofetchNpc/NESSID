07a1f31f6887c0602163f78b9a26b61b0d6081f4
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-30 14:47:10 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-import { downloadOtakuEpisode } from './index.js'; 
+/*import { downloadOtakuEpisode } from './index.js'; 
 
 const data = await downloadOtakuEpisode('dddn-episode-9-sub-indo');
 console.log(JSON.stringify(data, null, 2));
+*/
