e5bfb6838956391f7c2580bf8f4af922591a94ff
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-30 13:46:49 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import { generateBratImage } from './index.js'; 
+import { generateWaifuImage } from './index.js'; 
 
-const data = await generateBratImage('hallo');
+const data = await generateWaifuImage('cute anime girl');
 console.log(JSON.stringify(data, null, 2));
