0069de33357678c31dd5d2b0a78d3f4c7f2c4fcf
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-30 14:21:28 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 import { generateEmojiAIImage } from './index.js'; 
 
-const data = await generateEmojiAIImage('anime angry');
+const data = await generateEmojiAIImage('anime style angry with blush');
 console.log(JSON.stringify(data, null, 2));
