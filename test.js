82de8beda8b3a1b17909f3ce37e14c87524ee370
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-30 14:36:25 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import { searchOtakuSource } from './index.js'; 
+import { downloadOtakuEpisode } from './index.js'; 
 
-const data = await searchOtakuSource('date a live');
+const data = await downloadOtakuEpisode('date-live-v-sub-indo');
 console.log(JSON.stringify(data, null, 2));
