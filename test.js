8a8a6bb780961ba8da772c8493d11d45f5a4053b
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-10 12:01:25 +0700

    Update test.js

--- test.js
+++ test.js
@@ -3,3 +3,11 @@
 const data = await downloadOtakuEpisode('dddn-episode-9-sub-indo');
 console.log(JSON.stringify(data, null, 2));
 */
+
+import { baLogo } from './index.js';
+
+(async () => {
+    const logoText = "Kanjut Kanjut";
+    const response = await baLogo(logoText);
+    console.log(response);
+})();
