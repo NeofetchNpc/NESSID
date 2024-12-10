2708deedbf34994ebcbd68d46ef02dda641e9600
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-10 12:06:03 +0700

    Update index.js

--- plugins/index.js
+++ plugins/index.js
@@ -1,1 +1,23 @@
+// DOWNLOADER
+import fetch from 'node-fetch';
 
+export default async function YouTubeSearch(query) {
+    try {
+        const apiUrl = `https://api.neastooid.xyz/api/tools/yts?q=${encodeURIComponent(query)}`;
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.error('Error:', error.message);
+        return {
+            success: false,
+            results: [],
+            error: 'Error dalam mendapatkan data dari API YouTube',
+        };
+    }
+}
