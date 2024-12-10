add07e1ca128d34662337e0596196f88dda0675f
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-10 12:11:09 +0700

    Update index.js

--- plugins/index.js
+++ plugins/index.js
@@ -21,3 +21,36 @@ export default async function YouTubeSearch(query) {
         };
     }
 }
+
+// MAKER CANVAS
+import baLogoGenerator from 'ba-logo';
+import { promises as fs } from 'fs';
+import { tmpdir } from 'os';
+import { join } from 'path';
+
+export default async function baLogo(text) {
+    try {
+        // Generate logo menggunakan 'ba-logo' dengan teks yang diberikan
+        const image = await baLogoGenerator(text);
+
+        // Tentukan path file di direktori sementara
+        const tempPath = join(tmpdir(), `${text.replace(/\s+/g, '_')}.png`);
+
+        // Simpan file di direktori sementara
+        await image.toFile(tempPath);
+
+        // Kembalikan path file sementara sebagai hasil
+        return {
+            success: true,
+            results: {
+                tempPath, // Lokasi file sementara di direktori tmp
+            },
+        };
+    } catch (error) {
+        console.error('Error:', error.message);
+        return {
+            success: false,
+            error: 'Gagal membuat logo dengan ba-logo',
+        };
+    }
+}
