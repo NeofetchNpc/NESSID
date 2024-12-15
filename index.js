462403fedb6e2f4068d5e61eb0d645a20428e045
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-15 19:24:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import { chatGPT } from './plugins/Ai-ChatGPT.js';
+import { aioDown } from './plugins/DL-aio.js';
+import { bStasionDl } from './plugins/DL-bli.js';
+import { facebookDl } from './plugins/DL-facebook.js';
+import { chatGPT } from './plugins/Ai-ChatGPT.js';
+import { chatGPT } from './plugins/Ai-ChatGPT.js';
+import { chatGPT } from './plugins/Ai-ChatGPT.js';
 
 const nessid = {
     chatGPT,
+    aioDown,
+    bStasionDl,
+    facebookDl,
 };
 
 export default nessid;
