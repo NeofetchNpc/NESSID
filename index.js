148873b33834ba2ea3682921528d47057ef78b1a
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-15 19:26:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import { chatGPT } from './plugins/Ai-ChatGPT.js';
 import { aioDown } from './plugins/DL-aio.js';
 import { bStasionDl } from './plugins/DL-bli.js';
 import { facebookDl } from './plugins/DL-facebook.js';
-import { chatGPT } from './plugins/Ai-ChatGPT.js';
+import { InstagramDL } from './plugins/DL-instagram.js';
+import { gdriveDown } from './plugins/DL-gdrive.js';
+import { mediafireDl } from './plugins/DL-mediafire.js';
 import { chatGPT } from './plugins/Ai-ChatGPT.js';
 import { chatGPT } from './plugins/Ai-ChatGPT.js';
 
@@ -11,6 +13,9 @@ const nessid = {
     aioDown,
     bStasionDl,
     facebookDl,
+    InstagramDL,
+    gdriveDown,
+    mediafireDl,
 };
 
 export default nessid;
