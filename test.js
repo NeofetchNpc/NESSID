9d8d06afcce4a809dd379b840bc8b8199a13023f
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-27 14:43:05 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import nessid from './index.js';
 
 (async () => {
-    const chatResponse = await nessid.AioDL('https://www.instagram.com/reel/DDrvQO4Se0h/?igsh=MWVpMXVpa2oxYThzNg==');
-    console.log(chatResponse);
+    const testing = await nessid.bStationDL('https://www.instagram.com/reel/DDrvQO4Se0h/?igsh=MWVpMXVpa2oxYThzNg==');
+    console.log(testing);
 })();
