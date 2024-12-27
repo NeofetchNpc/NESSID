66a148770ab377e94b64c24aa1693c9808dcab18
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-27 14:20:33 +0700

    Update DL-facebook.js

--- plugins/DL-facebook.js
+++ plugins/DL-facebook.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
 
-export async function facebookDl(url) {
+export async function facebookDL(url) {
   if (!url) throw new Error('URL is required.');
 
-  const endpoint = `https://api.neastooid.xyz/api/downloader/fbdl?url=${encodeURIComponent(url)}`;
+  const endpoint = `https://api.ryzendesu.vip/api/downloader/fbdl?url=${encodeURIComponent(url)}`;
   const response = await fetch(endpoint);
 
   if (!response.ok) {
