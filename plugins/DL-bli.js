87fe111dc65ca59f42a5ec47ff13674ede828f41
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-27 15:01:49 +0700

    Update DL-bli.js

--- plugins/DL-bli.js
+++ plugins/DL-bli.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
 export async function bStationDL(url) {
-  if (!url) throw new Error('URL is required.');
+  if (!url) {
+    throw new Error('URL is required.');
+  }
 
   try {
     const { data } = await axios.get('https://api.ryzendesu.vip/api/downloader/bilibili', {
       params: { url },
     });
-    
-    return data?.data || {};
+
+    return data?.data;
   } catch (error) {
-    throw new Error(`Failed to fetch: ${error.response?.status || error.message}`);
+    throw new Error(`Failed to fetch: ${error.message}`);
   }
 }
