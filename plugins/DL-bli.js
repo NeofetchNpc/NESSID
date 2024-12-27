adb7e60ddbddc0c29347170f79959f6eba6c478f
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-27 14:51:55 +0700

    Update DL-bli.js

--- plugins/DL-bli.js
+++ plugins/DL-bli.js
@@ -4,13 +4,12 @@ export async function bStationDL(url) {
   if (!url) throw new Error('URL is required.');
 
   try {
-    const { data } = await axios.get(`https://api.ryzendesu.vip/api/downloader/bilibili`, {
+    const { data } = await axios.get('https://api.ryzendesu.vip/api/downloader/bilibili', {
       params: { url },
     });
-    return data;
+    
+    return data?.data || {};
   } catch (error) {
-    // Tambahkan log untuk debugging
-    console.error('Error Response:', error.response?.data || error.message);
     throw new Error(`Failed to fetch: ${error.response?.status || error.message}`);
   }
 }
