69300c13e02b4332490c727f8da046c06b07fd44
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-27 14:16:45 +0700

    Update DL-aio.js

--- plugins/DL-aio.js
+++ plugins/DL-aio.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-export async function AioDownloads(url) {
+export async function AioDL(url) {
   if (!url) throw new Error('URL is required.');
 
   const endpoint = `https://api.ryzendesu.vip/api/downloader/aiodown?url=${encodeURIComponent(url)}`;
