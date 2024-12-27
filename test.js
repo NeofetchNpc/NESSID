cf13d31efc3f59c5f5ec2a65f0234193802f86de
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-27 14:16:09 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import nessid from 'neastooapi';
 
 (async () => {
-    const chatResponse = await nessid.AioDL('link');
+    const chatResponse = await nessid.AioDL('https://www.instagram.com/reel/DDrvQO4Se0h/?igsh=MWVpMXVpa2oxYThzNg==');
     console.log(chatResponse);
 })();
