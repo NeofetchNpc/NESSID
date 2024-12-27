7761d7f55b396d93796bc37857e2a82afb7ed528
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-27 14:38:18 +0700

    Update DL-aio.js

--- plugins/DL-aio.js
+++ plugins/DL-aio.js
@@ -3,10 +3,8 @@ import axios from 'axios';
 export async function AioDL(url) {
   if (!url) throw new Error('URL is required.');
 
-  try {
-    const { data } = await axios.get(`https://api.ryzendesu.vip/api/downloader/aiodown?url=${encodeURIComponent(url)}`);
-    return data;
-  } catch (error) {
-    throw new Error(`Failed to fetch: ${error.response?.status || error.message}`);
-  }
+  const { data } = await axios.get(`https://api.ryzendesu.vip/api/downloader/aiodown`, {
+    params: { url },
+  });
+  return data;
 }
