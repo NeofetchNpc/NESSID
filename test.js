e87f6e7f7569ff433920fd4a603624bfe86c10ad
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-27 15:11:51 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 import nessid from './index.js';
 
-nessid.bStationDL('https://www.bilibili.tv/id/video/2043082458?bstar_from=bstar-web.ugc-video-detail.related-recommend.all')
-  .then(result => console.log(result))
-  .catch(error => console.error(error));
+const url = 'https://www.bilibili.tv/id/video/2043082458?bstar_from=bstar-web.ugc-video-detail.related-recommend.all';
+
+nessid.bStationDL(url)
+  .then(result => {
+    console.log(JSON.stringify(result, null, 2));
+  })
+  .catch(error => {
+    console.error('Terjadi kesalahan:', error);
+  });
