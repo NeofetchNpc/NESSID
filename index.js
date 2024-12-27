991b21fd520f5a1778ba62efd1fb36a02345c780
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-27 14:10:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-import { chatGPT } from './plugins/Ai-ChatGPT.js';
-import { aioDown } from './plugins/DL-aio.js';
-import { bStasionDl } from './plugins/DL-bli.js';
-import { facebookDl } from './plugins/DL-facebook.js';
+import { AioDownloads } from './plugins/DL-aio.js';
+import { bStationDL } from './plugins/DL-bli.js';
+import { facebookDL } from './plugins/DL-facebook.js';
 import { InstagramDL } from './plugins/DL-instagram.js';
-import { gdriveDown } from './plugins/DL-gdrive.js';
-import { mediafireDl } from './plugins/DL-mediafire.js';
-import { soundCloudDl } from './plugins/DL-soundcloud.js';
+import { gdriveDL } from './plugins/DL-gdrive.js';
+import { mediafireDL } from './plugins/DL-mediafire.js';
+import { SoundCloudDL } from './plugins/DL-soundcloud.js';
 import { SpotifyDL } from './plugins/DL-spotify.js';
 import { TiktokDL } from './plugins/DL-tiktok.js';
-import { twitterDl } from './plugins/DL-x.js';
-import { ytmp3Dl } from './plugins/DL-ytmp3.js';
-import { ytmp4Dl } from './plugins/DL-ytmp4.js';
-import { pixivTl } from './plugins/Tl-pixiv.js';
+import { twitterDL } from './plugins/DL-x.js';
+import { ytmp3DL } from './plugins/DL-ytmp3.js';
+import { ytmp4DL } from './plugins/DL-ytmp4.js';
 
 const nessid = {
-    chatGPT,
-    aioDown,
-    bStasionDl,
-    facebookDl,
+    AioDownloads,
+    bStationDL,
+    facebookDL,
     InstagramDL,
-    gdriveDown,
-    mediafireDl,
+    gdriveDL,
+    mediafireDL,
     SpotifyDL,
     TiktokDL,
-    twitterDl,
-    ytmp3Dl,
-    ytmp4Dl,
-    pixivTl,
+    twitterDL,
+    ytmp3DL,
+    ytmp4DL,
 };
 
 export default nessid;
