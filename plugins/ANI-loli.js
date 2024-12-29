56699270e70d5c861aaf1ab265879ede2798fbe0
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-29 18:56:07 +0000

    update ANI-loli.js

--- plugins/ANI-loli.js
+++ plugins/ANI-loli.js
@@ -1,1 +1,10 @@
+import axios from 'axios';
 
+export async function getAnimeLoli() {
+  try {
+    const { data } = await axios.get('https://api.neastooid.xyz/api/anime/loli');
+    return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
