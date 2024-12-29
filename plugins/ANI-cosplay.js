f3096b32361d600b1316684a1e8b957f5d36cad1
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-29 18:57:09 +0000

    update ANI-cosplay.js

--- plugins/ANI-cosplay.js
+++ plugins/ANI-cosplay.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function getAnimeLoli() {
   try {
-    const { data } = await axios.get('https://api.neastooid.xyz/api/anime/cospkayer');
+    const { data } = await axios.get('https://api.neastooid.xyz/api/anime/cosplayer');
     return data;
   } catch (error) {
     throw new Error(error.message);
