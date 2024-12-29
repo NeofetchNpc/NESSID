9ea669adab44168efcc4710f7990f19eec9a7e13
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-29 18:59:01 +0000

    update ANI-waifuv1.js

--- plugins/ANI-waifuv1.js
+++ plugins/ANI-waifuv1.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
 
+export async function getAnimeWithTag(tag = 'waifu') {
+  try {
+    const { data } = await axios.get('https://api.neastooid.xyz/api/anime/waifuv1', {
+      params: { included_tags: tag },
+    });
+    return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
