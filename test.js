9f8cf72568bd2f89b688ab3acf97857da3012a45
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-12-30 02:14:45 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
 import nessid from './index.js';
 
-const url = 'https://www.tiktok.com/@yusupkaku/video/7450672111032503557';
-
-nessid.TiktokDL(url)
-  .then(result => {
-    console.log(JSON.stringify(result, null, 2));
-  })
+nessid.TiktokDL('https://www.tiktok.com/@yusupkaku/video/7450672111032503557')
+  .then(result => console.log(result));
