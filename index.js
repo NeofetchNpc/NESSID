4870182cac93d64e0c76cb8ff3f05df30322ca0c
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-01 07:57:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import { SFWlolicon } from './plugins/ANI-loli.js';
 import { SFWwaifuv1 } from './plugins/ANI-waifuv1.js';
 import { SFWwaifuv2 } from './plugins/ANI-waifuv2.js';
 import { A1NSFx } from './plugins/AI-ainsfx.js';
+import { chatGpt } from './plugins/AI-gpt.js';
 import { baLogo } from './plugins/MK-bluearchive.js';
 
 const nessid = {
@@ -27,6 +28,7 @@ const nessid = {
   SFWlolicon,
   SFWcosplay,
   A1NSFx,
+  chatGpt,
   baLogo,
 };
 
