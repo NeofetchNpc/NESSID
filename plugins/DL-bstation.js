d804dcc5b56c6e4632dc5bc7d21f2a4323e0e45d
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-01 19:52:19 +0700

    Update DL-bstation.js

--- plugins/DL-bstation.js
+++ plugins/DL-bstation.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
 
+export async function twitterDL(url) {
+  if (!url) throw new Error('URL is required.');
+
+  try {
+    const { data } = await axios.get('https://api.neastooid.xyz/api/downloader/bstation', {
+      params: { url },
+    });
+
+    return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
