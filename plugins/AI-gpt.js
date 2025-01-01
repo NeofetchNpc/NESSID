c654811d4acfd9a7bba6661d5bf8d457355e4975
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-01 08:06:09 +0700

    Update AI-gpt.js

--- plugins/AI-gpt.js
+++ plugins/AI-gpt.js
@@ -1,19 +1,15 @@
 import axios from 'axios';
 
-export async function chatGpt(q) {
-  if (!q) {
-    throw new Error('Query parameter q is required.');
-  }
+export async function ChatGpt(q) {
+  if (!q) throw new Error('URL is required.');
 
   try {
-    const { data } = await axios.get(`https://api.neastooid.xyz/api/ai/gpt?q=${q}`);
-
-    if (!data.success) {
-      throw new Error(data.message || 'Something went wrong');
-    }
+    const { data } = await axios.get('https://api.neastooid.xyz/api/ai/gpt', {
+      params: { q },
+    });
 
     return data;
   } catch (error) {
-    throw new Error(error.response ? error.response.data.message : error.message || 'Unknown error');
+    throw new Error(error.message);
   }
 }
