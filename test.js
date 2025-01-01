9ff764727cc1178d834d03665b24a90717ad0a1d
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-01 07:37:08 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-import nessid from './index.js';
+/*import nessid from './index.js';
 
 nessid.baLogo('jawa')
+  .then(result => console.log(result));
+*/
+
+import nessid from './index.js';
+
+nessid.TiktokDL('https://www.tiktok.com/@yusupkaku/video/7450672111032503557')
   .then(result => console.log(result));
