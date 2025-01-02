3d6a8d8e2d74bfbdc0460eb6f55feb1270a6697e
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-02 08:06:26 +0700

    Update test.js

--- test.js
+++ test.js
@@ -4,3 +4,8 @@ import nessid from './index.js';
 nessid.SimiAi('haloo')
   .then(result => console.log(result));
 */
+
+import nessid from 'neastooapi';
+
+nessid.baLogo('ini text nya')
+  .then(result => console.log(result));
