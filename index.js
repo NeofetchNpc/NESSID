e6734c2c5e9b691a01d3dbddf5f0e4b2e409c71e
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-19 01:38:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-import { BstationDl } from './plugins/DL-bstation.js';
-import { facebookDL } from './plugins/DL-facebook.js';
-import { InstagramDL } from './plugins/DL-instagram.js';
-import { mediafireDL } from './plugins/DL-mediafire.js';
-import { TiktokDL } from './plugins/DL-tiktok.js';
-import { twitterDL } from './plugins/DL-x.js';
-import { ytmp3DL } from './plugins/DL-ytmp3.js';
-import { ytmp4DL } from './plugins/DL-ytmp4.js';
+import { fbDl } from './plugins/DL-facebook.js';
+import { igDl } from './plugins/DL-instagram.js';
+import { mefireDl } from './plugins/DL-mediafire.js';
+import { ttDl } from './plugins/DL-tiktok.js';
+import { xDl } from './plugins/DL-x.js';
+import { ytmp3Dl } from './plugins/DL-ytmp3.js';
+import { ytmp4Dl } from './plugins/DL-ytmp4.js';
 import { SFWcosplay } from './plugins/ANI-cosplay.js';
 import { SFWlolicon } from './plugins/ANI-loli.js';
-import { SFWwaifuv1 } from './plugins/ANI-waifuv1.js';
-import { SFWwaifuv2 } from './plugins/ANI-waifuv2.js';
+import { SFWaifuv1 } from './plugins/ANI-waifuv1.js';
+import { SFWaifuv2 } from './plugins/ANI-waifuv2.js';
 
 const nessid = {
-  BstationDl,
-  facebookDL,
-  InstagramDL,
-  mediafireDL,
-  TiktokDL,
-  twitterDL,
-  ytmp3DL,
-  ytmp4DL,
-  SFWwaifuv2,
-  SFWwaifuv1,
+  fbDl,
+  igDl,
+  mefireDl,
+  ttDl,
+  xDl,
+  ytmp3Dl,
+  ytmp4Dl,
+  SFWaifuv1,
+  SFWaifuv2,
   SFWlolicon,
   SFWcosplay,
 };
