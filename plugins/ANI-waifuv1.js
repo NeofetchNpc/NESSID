19f2be5f4aa4a870c669024fe187fcdcb1589997
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-19 01:41:48 +0700

    Update ANI-waifuv1.js

--- plugins/ANI-waifuv1.js
+++ plugins/ANI-waifuv1.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export async function SFWwaifuv1(tag = 'waifu') {
+export async function SFWaifuv1(tag = 'waifu') {
   try {
     const { data } = await axios.get('https://api.neastooid.xyz/api/anime/waifuv1', {
       params: { included_tags: tag },
