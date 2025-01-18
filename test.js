e2004c8373819edb88b758b4b4aa579e7716368f
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-18 11:56:29 +0700

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ nessid.SimiAi('haloo')
   .then(result => console.log(result));
 */
 
-import nessid from 'neastooapi';
+import nessid from './index.js';
 
 nessid.ytmp4DL('https://youtu.be/SDFIvvHzBeQ?si=5d6IuqoId-agyitP', '720p')
   .then(result => console.log(result));
