85b501df324a08c42bb7486794732e3406beda04
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-18 19:27:35 +0700

    Update DL-ytmp3.js

--- plugins/DL-ytmp3.js
+++ plugins/DL-ytmp3.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-export async function ytmp3DL(url) {
+export async function ytmp3DL(url, q = '128') {
   if (!url) throw new Error('URL is required.');
+  if (!q) throw new Error('Quality is required.');
 
   try {
     const { data } = await axios.get('https://api.neastooid.xyz/api/downloader/ytmp3', {
-      params: { url },
+      params: { url, q },
     });
 
     return data;
