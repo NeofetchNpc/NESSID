7a2f97a9d4bf3bbabbdd0d53e19f4a88ced47c0d
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-19 01:34:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { BstationDl } from './plugins/DL-bstation.js';
 import { facebookDL } from './plugins/DL-facebook.js';
 import { InstagramDL } from './plugins/DL-instagram.js';
 import { mediafireDL } from './plugins/DL-mediafire.js';
-import { SpotifyDL } from './plugins/DL-spotify.js';
 import { TiktokDL } from './plugins/DL-tiktok.js';
 import { twitterDL } from './plugins/DL-x.js';
 import { ytmp3DL } from './plugins/DL-ytmp3.js';
@@ -11,14 +10,12 @@ import { SFWcosplay } from './plugins/ANI-cosplay.js';
 import { SFWlolicon } from './plugins/ANI-loli.js';
 import { SFWwaifuv1 } from './plugins/ANI-waifuv1.js';
 import { SFWwaifuv2 } from './plugins/ANI-waifuv2.js';
-import { A1NSFx } from './plugins/AI-ainsfx.js';
 
 const nessid = {
   BstationDl,
   facebookDL,
   InstagramDL,
   mediafireDL,
-  SpotifyDL,
   TiktokDL,
   twitterDL,
   ytmp3DL,
@@ -27,7 +24,6 @@ const nessid = {
   SFWwaifuv1,
   SFWlolicon,
   SFWcosplay,
-  A1NSFx,
 };
 
 export default nessid;
