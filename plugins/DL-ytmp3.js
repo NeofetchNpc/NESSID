66d0d3ccbf015ba26916da94c2e2825b36c934d4
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-21 00:10:41 +0700

    Update DL-ytmp3.js

--- plugins/DL-ytmp3.js
+++ plugins/DL-ytmp3.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export async function ytmp3DL(url, quality = 128) {
+export async function YTMP3Dl(url, quality = 128) {
   if (!url) throw new Error('URL is required.');
 
   try {
