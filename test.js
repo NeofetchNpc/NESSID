8b364ce2ae39e574bc915301fa3529f927f4e689
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-01-24 11:53:29 +0700

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ nessid.ANIwallpaper('nakano miku')
   .then(result => console.log(result));
 */
 
-import nessid from 'neastooapi';
+import nessid from './index.js';
 
 nessid.TTDl('https://www.tiktok.com/@yusupkaku/video/7450672111032503557')
   .then(result => console.log(result));
