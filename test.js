c26f09d215bbd0c042ef6fab2bec5e9dce5003af
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-02-08 16:54:26 +0700

    Update test.js

--- test.js
+++ test.js
@@ -3,3 +3,8 @@
 nessid.GptAi('halo')
   .then(result => console.log(result));
 */
+
+import nessid from './index.js';
+
+nessid.TTDl('https://www.tiktok.com/@nonome_n/video/7466488821912309000?is_from_webapp=1&sender_device=pc')
+  .then(result => console.log(result));``
