ff69db7604174e36630b45a51def4be376fe7532
INsITdeveloper <153608709+INsITdeveloper@users.noreply.github.com>
2025-02-10 08:54:10 +0700

    Update AI-insfxv2.js

--- plugins/AI-insfxv2.js
+++ plugins/AI-insfxv2.js
@@ -6,13 +6,13 @@ export async function AiInsfxV2(imageUrl, model = 'anime') {
   }
 
   try {
-    const { data } = await axios.post('https://ins.neastooid.xyz/api/ai/ainsfxv2', 
+    const { data } = await axios.post('https://ins.balxzzy.web.id//api/ai/ainsfxv2', 
       { imageUrl, model }, 
       {
         headers: {
           'Content-Type': 'application/json',
           'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36',
-          'Referer': 'https://ins.neastooid.xyz/'
+          'Referer': 'https://ins.balxzzy.web.id//api/ai/ainsfxv2'
         }
       }
     );
