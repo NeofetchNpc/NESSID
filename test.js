fba0a431c44e0dfe9ce9f225d9f41495579c4466
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-02-28 15:45:50 +0700

    Update test.js

--- test.js
+++ test.js
@@ -3,9 +3,3 @@
 nessid.GptAi('halo')
   .then(result => console.log(result));
 */
-
-import nessid from './index.js';
-
-nessid.youtubeAIO('https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'audio')
-  .then(console.log)
-  .catch(error => console.error('Error:', error.message));
