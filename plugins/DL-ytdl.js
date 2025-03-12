1195fd03601094d1879c626156970d273f12cd03
YusupKakuu <galuhtzy404@gmail.com>
2025-03-12 08:14:35 +0700

    Update DL-ytdl.js

--- plugins/DL-ytdl.js
+++ plugins/DL-ytdl.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 import { domain } from '../index.js';
 
-export async function youtubeAIO(url, type = 'audio') {
+export async function youtubeAIO(url, type = 'ytmp3') {
   if (!url) {
     throw new Error('URL is required.');
   }
 
-  if (!['video', 'audio'].includes(type)) {
-    throw new Error('Invalid type. Use "video" or "audio".');
+  if (!['ytmp3', 'ytmp4'].includes(type)) {
+    throw new Error('Invalid type. Use "ytmp4" or "ytmp3".');
   }
 
   try {
-    const endpoint = `/api/youtube/${type}?url=${encodeURIComponent(url)}`;
+    const endpoint = `/api/downloader/ytdl/${type}?url=${encodeURIComponent(url)}`;
     const response = await axios.get(`${domain}${endpoint}`);
 
     return response.data; // Meneruskan JSON asli dari API
