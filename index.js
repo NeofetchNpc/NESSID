9fa680fcbc2994b8ee5f63f694108746a9ff603d
YusupKakuu <galuhtzy404@gmail.com>
2025-03-12 08:26:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 import { SoundCloudDl } from './plugins/DL-soundcloud.js';
 import { FBDl } from './plugins/DL-facebook.js';
 import { IGDl } from './plugins/DL-instagram.js';
-import { MediafireDl } from './plugins/DL-mediafire.js';
 import { TTDl } from './plugins/DL-tiktok.js';
 import { XDl } from './plugins/DL-x.js';
-import { SpotifyDl } from './plugins/DL-spotify.js';
 import { youtubeAIO } from './plugins/DL-ytdl.js';
 import { SFWcosplay } from './plugins/ANI-cosplay.js';
 import { SFWlolicon } from './plugins/ANI-loli.js';
 import { SFWaifu } from './plugins/ANI-waifuv1.js';
 import { PinSearch } from './plugins/ANI-pin.js';
 import { ANIwallpaper } from './plugins/ANI-wallpaper.js';
-import { GptAi } from './plugins/AI-gpt4o.js';
 import { AiInsfxV2 } from './plugins/AI-insfxv2.js';
-import { GdriveDL } from './plugins/DL-gdrive.js';
 import { OTTnew } from './plugins/OTT-new.js';
 import { OTTepid } from './plugins/OTT-epid.js';
 import { OTTsearch } from './plugins/OTT-search.js';
@@ -25,12 +21,9 @@ const nessid = {
   OTTinfo,
   OTTsearch,
   AiInsfxV2,
-  GptAi,
-  GdriveDL,
   SoundCloudDl,
   FBDl,
   IGDl,
-  MediafireDl,
   TTDl,
   XDl,
   SpotifyDl,
