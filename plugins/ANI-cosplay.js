8bf3c6f0c704751e43865dbcd7766e0f66a4d062
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-03-12 08:04:24 +0700

    Update ANI-cosplay.js

--- plugins/ANI-cosplay.js
+++ plugins/ANI-cosplay.js
@@ -3,7 +3,7 @@ import { domain } from '../index.js';
 
 export async function SFWcosplay() {
   try {
-    const { data } = await axios.get(`${domain}/api/cosplayer`);
+    const { data } = await axios.get(`${domain}/api/anime/cosplay`);
     return data;
   } catch (error) {
     throw new Error(error.message);
