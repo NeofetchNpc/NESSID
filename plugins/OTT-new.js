a00aa9652d16a4f75dbe1d9e3927c9f335680aef
YusupKakuu <galuhtzy404@gmail.com>
2025-03-12 08:15:43 +0700

    Update OTT-new.js

--- plugins/OTT-new.js
+++ plugins/OTT-new.js
@@ -3,7 +3,7 @@ import { domain } from '../index.js';
 
 export async function OTTnew() {
   try {
-    const { data } = await axios.get(`${domain}/api/an-new`);
+    const { data } = await axios.get(`${domain}/api/otaku/an-new`);
     return data;
   } catch (error) {
     throw new Error(error.message);
