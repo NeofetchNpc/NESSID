7511169dc9d1958ccac667b6c86b44c8acf2dc3c
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2025-03-12 08:03:11 +0700

    Update AI-insfxv2.js

--- plugins/AI-insfxv2.js
+++ plugins/AI-insfxv2.js
@@ -1,24 +1,21 @@
 import axios from 'axios';
 
-export async function AiInsfxV2(imageUrl, model = 'anime') {
-  if (!['anime', 'pixar'].includes(model)) {
-    throw new Error('Model harus "anime" atau "pixar"');
-  }
-
+export async function AiInsfxV2(imageUrl, model = 'instex2img', apikey = 'yusup') {
   try {
-    const { data } = await axios.post('https://ins.balxzzy.web.id//api/ai/ainsfxv2', 
-      { imageUrl, model }, 
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36',
-          'Referer': 'https://ins.balxzzy.web.id//api/ai/ainsfxv2'
-        }
+    const { data } = await axios.get(`https://apii.ins.biz.id/api/ai/ainsfxv2`, {
+      params: {
+        q: imageUrl,
+        model: model,
+        apikey: apikey
+      },
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36',
+        'Referer': 'https://apii.ins.biz.id/api/ai/ainsfxv2'
       }
-    );
+    });
 
     return data.uploadedUrl || null;
   } catch (error) {
-    throw new Error(error.message);
+    throw new Error(error.response?.data?.message || error.message);
   }
 }
